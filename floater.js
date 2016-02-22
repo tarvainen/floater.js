a4c962d67dbba41639a3b07dcbe061165bf2bc8f
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-02-22 11:03:49 +0200

    Add custom message support.

--- floater.js
+++ floater.js
@@ -28,21 +28,35 @@ Floater.prototype.getElements = function () {
 Floater.prototype.createElements = function () {
 	for (var i = 0; i < this.elements.length; i++) {
 		var el = this.elements[i];
-		var container = document.createElement('div');
-		container.className = 'floater-container';
+		var container = this.createContainer(el);
 		el.parentNode.insertBefore(container, el.nextSibling);
-		var lbl = this.createLabel(el);
+
+		var lbl = this.createLabel('floater-label', el);
+		var error = this.createLabel('floater-message');
 		container.appendChild(lbl);
+		container.appendChild(error);
 		container.appendChild(el);
-		lbl.className += ' hidden';
+
+		this.hide(lbl);
 		this.bindEvent(el, lbl);
 	}
 }
 
-Floater.prototype.createLabel = function (element) {
+Floater.prototype.createContainer = function (el) {
+	var container = document.createElement('div');
+	container.setAttribute('floater-id', el.getAttribute('floater-id'));
+	el.removeAttribute('floater-id');
+	container.className = el.className.replace(/floater/g, '');
+	container.className += ' floater-container';
+	return container;
+}
+
+Floater.prototype.createLabel = function (className, element) {
 	var node = document.createElement('span');
-	node.className = 'floater-label';
-	node.innerHTML = this.getText(element);
+	node.className = className;
+	if (element) {
+		node.innerHTML = this.getText(element);
+	}
 	return node;
 }
 
@@ -52,18 +66,16 @@ Floater.prototype.bindEvent = function (element, label) {
 			return;
 		}
 
-		label.className = label.className.replace(/hidden/g, '');
-		label.className += ' visible';
-	}).bind(label, element));
+		this.show(label);
+	}).bind(this));
 
 	element.addEventListener('blur', (function () {
 		if (element.value.length > 0) {
 			return;
 		}
 
-		label.className = label.className.replace(/visible/g, '');
-		label.className += 'hidden';
-	}).bind(label, element));
+		this.hide(label);
+	}).bind(this));
 }
 
 Floater.prototype.getText = function (element) {
@@ -82,4 +94,46 @@ Floater.prototype.float = function () {
 	}
 
 	this.createElements();
+	return this;
+}
+
+Floater.prototype.message = function (opts) {
+	if (!opts || !opts.id || !opts.text) {
+		return;
+	}
+
+	opts.type = opts.type || 'good';
+
+	var container = document.querySelector('[floater-id="' + opts.id + '"]');
+	var textField = container.getElementsByClassName(this.opts.class)[0];
+	var el = container.getElementsByClassName('floater-message')[0];
+
+	if (!el) {
+		return;
+	}
+
+	el.innerHTML = this.escapeHtml(opts.text);
+	this.show(el);
+	el.className += ' ' + opts.type;
+
+	textField.addEventListener('keyup', (function () {
+		this.hide(el);
+	}).bind(this));
+}
+
+Floater.prototype.hide = function (el) {
+	el.className = el.className.replace(/visible/g, '') + ' hidden';
+}
+
+Floater.prototype.show = function (el) {
+	el.className = el.className.replace(/hidden/g, '') + ' visible';
+}
+
+Floater.prototype.escapeHtml = function (html) {
+	return html
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
 }
